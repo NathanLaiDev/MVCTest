453174f830e8a98df165dd414953e969ff651172
Nathan Lai <z5209733@ad.unsw.edu.au>
2020-07-25 17:00:00 +1000

    Add project files.

---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,31 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            PersonModel p = new PersonModel("Nathan", "ls", 0);
+
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List <PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel("Nathan", "Lai", 20));
+            people.Add(new PersonModel("Tatsuya", "Shiba", 21));
+            people.Add(new PersonModel("Peppa", "Oink", 2));
+
+            return View(people);
+
+        }
+    }
+}
